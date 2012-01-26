aca0c3f64b1d4ad72a8bb1fde78c0bbc20347165
Hal Blackburn <hal.blackburn@gmail.com>
2012-01-26 11:49:57 +0000

    These changes implement the ical stuff from the previous commit.
    I forgot to add them to the last commit!

--- javascript/lecturelist.js
+++ javascript/lecturelist.js
@@ -17,8 +17,11 @@
  */
 
 // load the master sakai object to access all Sakai OAE API methods
-require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
-     
+require(["jquery", 
+         "sakai/sakai.api.core", 
+         "/devwidgets/lecturelist/javascript/jquery.icalendar.js"], 
+        function($, sakai) {
+	
     /**
      * @name sakai.WIDGET_ID
      *
@@ -42,111 +45,154 @@ require(["jquery", "sakai/sakai.api.core"], function($, sakai) {
         var $mainContainer = $("#lecturelist_main", $rootel);
         var $settingsContainer = $("#lecturelist_settings", $rootel);
         
-        var BASE_URL = "http://localhost/php/calendar2.php?tripos-part="
-        var TRIPOS_PART_ID = "T0024001692011";
-        var SUBJECT_ID = "T0024001692011003";
+        var BASE_URL = "http://localhost/php/gcal.php"
+        var SUBJECT_ID = "T0024001692011001,T0024001692011012";
         
         var LECTURE_ENTRY = $("#lecturelist_templates .entry", $rootel);
+        var AGENDA_ROW = $("#lecturelist_templates .agenda-row", $rootel);
 
+        var BSLASH_REGEX = new RegExp('\\\\', 'g');
+        
+        DAYS = {"0": "Mon",
+        		"1": "Tue",
+        		"2": "Wed",
+        		"3": "Thu",
+        		"4": "Fri",
+        		"5": "Sat",
+        		"6": "Sun"};
+
+        MONTHS = {"0": "Jan",
+        		  "1": "Feb",
+        		  "2": "Mar",
+        		  "3": "Apr",
+        		  "4": "May",
+        		  "5": "Jun",
+        		  "6": "Jul",
+        		  "7": "Aug",
+        		  "8": "Sep",
+        		  "9": "Oct",
+        		  "10": "Nov",
+        		  "11": "Dec"};
+        
         ///////////////////////
         // Utility functions //
         ///////////////////////
         
-        /**
-         * Extract the information on a specific subject from a raw timetables
-         * calendar (cal_*.json) document.
-         * 
-         * @param subjectId The ID of the subject to obtain.
-         * @param calendar The calendar data.
-         */
-        function extractSubjectData(calendar, subjectId) {
-        	var lectures = flattenSublists(calendar.rectangles);
-        	
-        	var subjectLectures = $.grep(lectures, function(l) {
-        		return l.cid && l.cid === subjectId;
-        	});
-        	
-        	var lectureData = $.map(subjectLectures, function(l) {
-        		return {
-        			title: l.what,
-        			datetime: l.when,
-        			location: l.where,
-        			type: l.type,
-        			presenter: l.organiser
-        		}
-        	});
-        	
-        	return {
-        		subject: calendar.courses[subjectId].name,
-        		lectures: lectureData
-        	}
-        }
-        
-        function flattenSublists(list, dest) {
-        	if(!dest)
-        		dest = [];
-        	
-        	for(var i = 0; i < list.length; ++i) {
-        		var entry = list[i];
-        		if($.type(entry) === "array") {
-        			flattenSublists(entry, dest);
-        		}
-        		else {
-        			dest.push(entry);
-        		}
-        	}
-        	return dest;
-        }
-        
         function fetchCalendarData(triposPartId, subjectId) {
         	var success = function(data) {
-        		showLectures(extractSubjectData(data, subjectId));
+        		var ical = $.icalendar.parse(data.calendardata);
+        		var days = groupByDay(
+        				$.grep(ical.vevent, notBefore(stripTime(new Date()))));
+        		showCalendar({name: ical["x-wr-calname"]._value, days: days});
         	};
         	
         	$.ajax({
-        		url: buildCalFeedUrl(triposPartId), 
-        		dataType: 'jsonp', 
+        		url: BASE_URL,
+        		data: {course: SUBJECT_ID, type: "ical"},
+        		dataType: 'jsonp',
         		success: success});
         }
         
-        function buildCalFeedUrl(triposPartId) {
-        	return BASE_URL + triposPartId;
+        function notBefore(date) {
+        	return function(event) {
+        		return event.dtstart >= date;
+        	}
         }
-
+        
+        function groupByDay(vevents) {
+        	var days = {}
+        	for(var i = 0; i < vevents.length; ++i) {
+        		var event = vevents[i];
+        		// We need a string to key our obj with
+        		var dateKey = stripTime(event.dtstart).toISOString();
+        		if(!days[dateKey]) {
+        			days[dateKey] = []
+        		}
+        		days[dateKey].push(event);
+        	}
+        	var sortedDays = [];
+        	for(key in days) {
+        		sortedDays.push([key, days[key]]);
+        	}
+        	sortedDays.sort(function(a, b) {
+        		if(a[0] < b[0])
+        			return -1;
+        		else if(a[0] > b[0])
+        			return 1;
+        		return 0;
+        	});
+        	
+        	return sortedDays;
+        }
+        
+        function stripTime(date) {
+        	return new Date(date.getFullYear(), date.getMonth(), 
+        			date.getDate());
+        }
+        
         /////////////////////////
         // Main View functions //
         /////////////////////////
 
         /** Called when the timetable lecture data has been recieved. */
-        function showLectures(lectures) {
-        	// FIXME: Update UI
-        	$(".lecture-subject", $rootel).text("Lectures of " + lectures.subject);
-        	var entries = lectures.lectures;
-        	var entryContainer = $("#lecturelist_lectures", $rootel);
-        	for(var i = 0; i < entries.length; ++i) {
-        		var entryData = entries[i];
-        		var entryHtml = LECTURE_ENTRY.clone();
-        		populateEntry(entryData, entryHtml).appendTo(entryContainer);
+        function showCalendar(calendar) {
+        	$(".lecture-subject", $rootel).text("Lectures of " + calendar.name);
+        	var agendaTable = $("#lecturelist_lectures #agenda", $rootel);
+        	console.log(agendaTable);
+        	for(var i = 0; i < calendar.days.length; ++i) {
+        		var day = calendar.days[i];
+        		var dayDateStr = day[0];
+        		var entries = day[1];
+        		appendDaysToTable(agendaTable, new Date(dayDateStr), entries);
         	}
         	
         	$(".loading", $rootel).hide();
         	$(".ajax-content", $rootel).show();
         }
         
-        function populateEntry(entryData, entryHtml) {
-        	$(".lecture-title", entryHtml).text(entryData.title);
-        	$(".lecture-presenter", entryHtml).text(entryData.presenter);
-        	$(".lecture-datetime", entryHtml).text(entryData.datetime);
-        	$(".lecture-location", entryHtml).text(entryData.location);
-        	return entryHtml;
+        /**
+         * @param table A JQuery wrapper of the element to insert the days into.
+         * @param day A Date object representing the day.
+         * @param events A list of events occuring on a single day.
+         */
+        function appendDaysToTable(table, day, events) {
+        	var root = $("<tbody>");
+        	for(var i = 0; i < events.length; ++i) {
+        		var event = events[i];
+        		var tr = buildRow(event);
+        		if(i === 0) { // first
+        			tr.prepend($("<td class='date'>")
+        					.text(buildDateString(day))
+        					.attr("rowspan", events.length));
+        		}
+        		
+        		root.append(tr);
+        	}
+        	table.append(root);
         }
         
-        /** Called when the HTTP GET for the timetable data fails. */
-        function showFetchLecturesFailed() {
-        	// FIXME: show proper UI message
-        	var msg = "Error fetching data from Timetables server.";
-        	alert(msg);
-        	console.log(msg);
+        function buildRow(event) {
+        	var root = $("<tr>");
+        	root.append($("<td class='time'>").text(
+        			buildTimeString(event.dtstart)));
+        	root.append($("<td class='description'>").text(
+        			event.summary.replace(BSLASH_REGEX, "")));
+        	return root;
+        }
+        
+        function buildTimeString(date) {
+        	var hour = date.getHours();
+        	var minute = "" + date.getMinutes();
+        	if(minute.length === 1)
+        		minute = "0" + minute;
+        	return hour + ":" + minute;
+        }
+        
+        function buildDateString(date) {
+        	var dayName = DAYS[date.getDay()];
+        	var dayNumber = date.getDate();
+        	var monthName = MONTHS[date.getMonth()];
+        	return dayName + " " + dayNumber + " " + monthName;
         }
 
         /////////////////////////////
